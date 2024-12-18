cfec124ebc1dd5d5ed4f1ec1779ce7744ea37205
Panyakorn Timchanthuek <sampeetiyoo4088@gmail.com>
2024-12-19 02:20:13 +0700

    del: functions

--- ui/test3.py
+++ ui/test3.py
@@ -1,14 +1,2 @@
 def subtract(a, b):
     return a / b
-
-def power(a, b):
-    return a ** b
-
-def sum(a, b):
-    return a + b
-
-def sigmoid(z):
-    return np.exp(-z)
-
-def linear_comb(x, w, b):
-    return w * x + b
