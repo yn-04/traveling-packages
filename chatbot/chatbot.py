479d85c3737ed743d3db61537f311671969c48cb
Panyakorn Timchanthuek <sampeetiyoo4088@gmail.com>
2024-12-30 10:12:37 +0000

    update: avaliable modules and instruction

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,21 +1,36 @@
 from groq import Groq
 
+# avaliable models
+models = {
+    "gemma2-9b-it": "gemma2-9b-it",
+    "mixtral-8x7b-32768": "mixtral-8x7b-32768",
+    "llama-3.2-1b-preview": "llama-3.2-1b-preview",
+    "llama-3.2-3b-preview": "llama-3.2-3b-preview",
+    "llama-3.1-8b-instant": "llama-3.1-8b-instant",
+    "llama3-groq-8b-8192-tool-use-preview": "llama3-groq-8b-8192-tool-use-preview",
+    "llama3-8b-8192": "llama3-8b-8192",
+    "llama-3.2-11b-vision-preview": "llama-3.2-11b-vision-preview",
+    "llama3-70b-8192": "llama3-70b-8192",
+    "llama-3.3-70b-specdec": "llama-3.3-70b-specdec",
+    "llama-3.3-70b-versatile": "llama-3.3-70b-versatile",
+    "llama-3.2-90b-vision-preview": "llama-3.2-90b-vision-preview",
+    "llama-3.1-70b-versatile": "llama-3.1-70b-versatile"
+}
+
+# api key
 groq_client = Groq(
     api_key="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",
 )
 
-llama_70B = "llama-3.1-70b-versatile"
-llama_405B = "llama-3.1-405b-reasoning"
-llama_8b = "llama3-8b-8192"
-
+# agent requests
 chat_completion = groq_client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "แนะนำสถานที่ท่องเที่ยวในประเทศไทย เป็นภาษาไทย",
+            "content": "แนะนำสถานที่ท่องเที่ยวจังหวัดขอนแก่นในประเทศไทย เป็นภาษาไทย โดยจัดแผนการท่องเที่ยวในจังหวัดขอนแก่นหลาย ๆ ตัวเลือกและมีความน่าสน", # จะให้ agent ตอบแบบไหนอยู่ที่การตั้ง instruction ตรงนี้
         }
     ],
-    model=llama_8b,
+    model=models['gemma2-9b-it'],
 )
 
 print(chat_completion.choices[0].message.content)
