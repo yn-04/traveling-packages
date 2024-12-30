7793822264d393f2c181cce7315daa7702072be7
modpanyakorn <panyakorn.mod@gmail.com>
2024-12-31 00:36:17 +0700

    update: code

--- gen-images/hello.py
+++ gen-images/hello.py
@@ -5,20 +5,13 @@ import os  # used to access filepaths
 from PIL import Image  # used to print and edit images
 from dotenv import load_dotenv
 
-
 load_dotenv()
 # initialize OpenAI client
+print(os.environ.get("OPENAI_API_KEY"))
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-image_dir_name = "images"
-image_dir = os.path.join(os.curdir, image_dir_name)
-
-if not os.path.isdir(image_dir):
-    os.mkdir(image_dir)
-# create an image
-
 # set the prompt
-prompt = "อุทยานแห่งชาติภูสวนทราย จังหวัดเลย ประเทศไทย"
+prompt = "รวมสถานที่ท่องเที่ยวในจังหวัดนครราชสีมาประเทศไทย"
 
 # call the OpenAI API
 generation_response = client.images.generate(
@@ -32,11 +25,20 @@ generation_response = client.images.generate(
 # print response
 print(generation_response)
 
-# save the image
+# create a directory to store the images into this working directory path
+image_dir_name = "images"
+image_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), image_dir_name)
+if not os.path.isdir(image_dir):
+    os.mkdir(image_dir)
+    
+# create the image file
 generated_image_name = "generated_image.png"  # any name you like; the filetype should be .png
 generated_image_filepath = os.path.join(image_dir, generated_image_name)
+
+# generate the image
 generated_image_url = generation_response.data[0].url  # extract image URL from response
 generated_image = requests.get(generated_image_url).content  # download the image
 
+# write image to file
 with open(generated_image_filepath, "wb") as image_file:
     image_file.write(generated_image)  # write the image to the file
